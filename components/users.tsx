fb2124878defd7bbe11bf613e86e6aa545a58cd1
xiaoycailin <denisprasetio060@gmail.com>
2024-04-23 22:19:04 +0700

    fix type user

--- components/users.tsx
+++ components/users.tsx
@@ -1,11 +1,12 @@
 import { prisma } from "@/lib/prisma"
+import type { Users } from "@prisma/client"
 import { Fragment } from "react"
 
 export async function Users() {
     const users = await prisma.users.findMany()
     return (
         <Fragment>
-            {users.map((user) => (
+            {users.map((user: Users) => (
                 <li key={user.id}>{user.name}</li>
             ))}
         </Fragment>
