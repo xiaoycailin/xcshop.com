4f387513914752a5aa379bf732aa371115af9917
xiaoycailin <denisprasetio060@gmail.com>
2024-05-03 18:55:46 +0700

    domain fix

--- client/brand/index.ts
+++ client/brand/index.ts
@@ -5,7 +5,7 @@ import { fetcher } from "@/lib/fetcher";
 export class brand {
     static async find(brandId: string) {
         try {
-            const brandData = await fetcher<Response<Brand>>('http://localhost:3000/api/brand/' + brandId, { method: 'GET' })
+            const brandData = await fetcher<Response<Brand>>('/api/brand/' + brandId, { method: 'GET' })
             return brandData
         } catch (error) {
             console.log(error)
